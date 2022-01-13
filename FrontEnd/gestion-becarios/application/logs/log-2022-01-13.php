3880cd958d37547d9da4de67053ded4411e9a563
Santiago Alejandro Vargas Céspedes <santiavctf@gmail.com>
2022-01-12 21:53:24 -0400

    Login with G

---
+++ FrontEnd/gestion-becarios/application/logs/log-2022-01-13.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-01-13 00:36:57 --> Severity: 8192 --> setcookie(): Passing null to parameter #2 ($value) of type string is deprecated C:\xampp\htdocs\gestion-becarios\system\libraries\Session\Session_driver.php 132
